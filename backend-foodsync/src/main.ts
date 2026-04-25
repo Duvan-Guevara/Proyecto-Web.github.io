6d993cf78674382207baba565c28f5d5b050edc9
Duvan-Guevara <jhonnier.guevara00@usc.edu.co>
2026-04-25 20:49:38 +0000

    agregando backend

--- backend-foodsync/src/main.ts
+++ backend-foodsync/src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: '*', // 👈 permite cualquier origen (para desarrollo)
+    origin: '*', 
   });
 
   await app.listen(3000, '0.0.0.0');
